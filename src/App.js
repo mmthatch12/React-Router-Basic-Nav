bf1dfc042c569f851939340f4f36c8608e7e2647
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-22 13:26:37 -0600

    wrapped app in router tags

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-import { Router } from "react-router-dom"
 
 const App = () => (
   <div>
