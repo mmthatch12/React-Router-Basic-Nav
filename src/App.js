f72abd018522216ddf2ed9ed7aea942d086ca88b
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-22 13:46:43 -0600

    MVP + Switch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 const App = () => (
   <div>
+
     <Navigation />
-    <Route path="/" exact component={Home} />
-    <Route Path="/about" component={About} />
-    <Route Path="/contact" component={Contact} />
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route Path="/about" component={About} />
+      <Route Path="/contact" component={Contact} />
+    </Switch>
   </div>
 );
 
