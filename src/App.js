52a922d97c852574286d5fb358f2c25e9bc8746e
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-22 13:22:27 -0600

    imported router to index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Router } from "react-router-dom"
 
 const App = () => (
   <div>
